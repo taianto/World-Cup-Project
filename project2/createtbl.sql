106538721c388ea4dad2f1cfbe7b6fb5d4b7ffc2
taianto <antont21c@gmail.com>
2023-02-22 17:20:44 -0500

    Created Most Tables: V1

--- project2/createtbl.sql
+++ project2/createtbl.sql
@@ -1,1 +1,184 @@
-hello hello world world
+-- Include your create table DDL statements in this file.
+-- Make sure to terminate each statement with a semicolon (;)
+
+-- LEAVE this statement on. It is required to connect to your database.
+CONNECT TO cs421;
+
+-- Remember to put the create table ddls for the tables with foreign key references
+--    ONLY AFTER the parent tables has already been created.
+
+-- This is only an example of how you add create table ddls to this file.
+--   You may remove it.
+CREATE TABLE Stadium
+(stadium_name VARCHAR(30) NOT NULL,
+ location VARCHAR(30) NOT NULL,
+ capacity INTEGER,
+ PRIMARY KEY (stadium_name));
+
+CREATE TABLE Client
+(email VARCHAR(30) NOT NULL,
+ first_name VARCHAR(30) NOT NULL,
+ last_name VARCHAR(15) NOT NULL,
+ PRIMARY KEY (email));
+
+CREATE TABLE Team
+(country VARCHAR(15) NOT NULL,
+ name VARCHAR(30) NOT NULL,
+ website_url VARCHAR(15) NOT NULL,
+ group VARCHAR(1) NOT NULL,
+ PRIMARY KEY (country));
+
+CREATE TABLE Referee
+(referee_id INTEGER NOT NULL,
+ name VARCHAR(30) NOT NULL,
+ experience INTEGER,
+ nationality VARCHAR(15),
+ PRIMARY KEY (referee_id));
+
+CREATE TABLE Seat
+(seat_id VARCHAR(5) NOT NULL,
+ stadium_name VARCHAR(30) NOT NULL,
+ PRIMARY KEY (seat_id, stadium_name),
+ FOREIGN KEY  (stadium_name)
+     REFERENCES Stadium);
+
+CREATE TABLE Ticket
+(seat_id VARCHAR(5) NOT NULL,
+ stadium_name VARCHAR(30) NOT NULL,
+ match_id INTEGER NOT NULL,
+ price FLOAT,
+ status VARCHAR(10) NOT NULL,
+ PRIMARY KEY (seat_id, stadium_name,match_id),
+ FOREIGN KEY  (seat_id, stadium_name)
+     REFERENCES Seat,
+ FOREIGN KEY (match_id)
+     REFERENCES Match);
+
+CREATE TABLE Transaction
+(seat_id VARCHAR(5) NOT NULL,
+ stadium_name VARCHAR(30) NOT NULL,
+ match_id INTEGER NOT NULL,
+ transaction_id INTEGER NOT NULL,
+ PRIMARY KEY (seat_id, stadium_name,match_id),
+ FOREIGN KEY (seat_id, stadium_name,match_id)
+     REFERENCES Ticket,
+ FOREIGN KEY (transaction_id)
+     REFERENCES Receipt);
+
+CREATE TABLE Receipt
+(transaction_id INTEGER NOT NULL,
+ total_cost FLOAT NOT NULL,
+ email VARCHAR(30) NOT NULL,
+ PRIMARY KEY (transaction_id),
+ FOREIGN KEY (email)
+     REFERENCES Client);
+
+CREATE TABLE Match
+(match_id INTEGER NOT NULL,
+ date DATE NOT NULL,
+ time INTEGER NOT NULL,
+ round VARCHAR(15) NOT NULL,
+ stadium_name NOT NULL,
+ PRIMARY KEY (match_id),
+ FOREIGN KEY (stadium_name)
+     REFERENCES Stadium);
+
+CREATE TABLE Played_Match
+(match_id INTEGER NOT NULL,
+ duration TIME NOT NULL,
+ PRIMARY KEY (match_id),
+ FOREIGN KEY (match_id)
+     REFERENCES Match);
+
+CREATE TABLE Match_Participants
+(match_id INTEGER NOT NULL,
+ country VARCHAR(15) NOT NULL,
+ PRIMARY KEY (match_id, country),
+ FOREIGN KEY (match_id)
+     REFERENCES Match,
+ FOREIGN KEY (country)
+     REFERENCES Team);
+
+CREATE TABLE Player
+(country VARCHAR(15) NOT NULL,
+ shirt_num INTEGER NOT NULL,
+ name VARCHAR(30) NOT NULL,
+ dob DATE NOT NULL,
+ general_position VARCHAR(15) NOT NULL,
+ PRIMARY KEY (country, shirt_num),
+ FOREIGN KEY (country)
+     REFERENCES Team);
+
+CREATE TABLE Player
+(country VARCHAR(15) NOT NULL,
+ shirt_num INTEGER NOT NULL,
+ name VARCHAR(30) NOT NULL,
+ dob DATE NOT NULL,
+ general_position VARCHAR(15) NOT NULL,
+ PRIMARY KEY (country, shirt_num),
+ FOREIGN KEY (country)
+     REFERENCES Team);
+
+CREATE TABLE Coach
+(country VARCHAR(15) NOT NULL,
+ role VARCHAR(15) NOT NULL,
+ name VARCHAR(30) NOT NULL,
+ dob DATE NOT NULL,
+ PRIMARY KEY (country, role),
+ FOREIGN KEY (country)
+     REFERENCES Team);
+
+CREATE TABLE Refereeship
+(referee_id INTEGER NOT NULL,
+ match_id INTEGER NOT NULL,
+ role VARCHAR(15) NOT NULL,
+ PRIMARY KEY (referee_id, match_id),
+ FOREIGN KEY (referee_id)
+     REFERENCES Referee,
+ FOREIGN KEY (match_id)
+     REFERENCES Match);
+
+CREATE TABLE PlaysIn
+(country VARCHAR(15) NOT NULL,
+ shirt_num INTEGER NOT NULL,
+ match_id INTEGER NOT NULL,
+ specific_pos VARCHAR(15) NOT NULL,
+ entry_time TIME NOT NULL,
+ exit_time TIME NOT NULL,
+ PRIMARY KEY (country, shirt_num, match_id),
+ FOREIGN KEY (country, shirt_num)
+     REFERENCES Player,
+ FOREIGN KEY (match_id)
+     REFERENCES Played_Match);
+
+CREATE TABLE Goal
+(occurrence INTEGER NOT NULL,
+ match_id INTEGER NOT NULL,
+ country VARCHAR(15) NOT NULL,
+ shirt_num INTEGER NOT NULL,
+ minute TIME NOT NULL,
+ is_penalty INTEGER NOT NULL,
+ PRIMARY KEY (match_id, occurrence),
+ FOREIGN KEY (match_id)
+     REFERENCES Played_Match,
+ FOREIGN KEY (country, shirt_num)
+     REFERENCES Player);
+
+CREATE TABLE Booking
+(country VARCHAR(15) NOT NULL,
+ shirt_num INTEGER NOT NULL,
+ referee_id INTEGER NOT NULL,
+ match_id INTEGER NOT NULL,
+ is_red INTEGER NOT NULL,
+ yellow_occurance INTEGER,
+ PRIMARY KEY (country, shirt_num, referee_id, match_id),
+ FOREIGN KEY (country, shirt_num)
+     REFERENCES Player,
+ FOREIGN KEY (referee_id)
+     REFERENCES Referee,
+ FOREIGN KEY (match_id)
+     REFERENCES Played_Match);
+
+
+
+
