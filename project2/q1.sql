03360b430c934ab0166b84dd51dd84e6251af9b3
Arizoba <atonscu@gmail.com>
2023-02-25 00:55:01 -0500

    first

--- project2/q1.sql
+++ project2/q1.sql
@@ -1,2 +1,10 @@
-SELECT *
-FROM
+Select * from
+    Stadium join Match on STADIUM.STADIUM_NAME = MATCH.STADIUM_NAME
+where MATCH_ID in
+
+(Select match_id from
+      Player join Goal on match_id
+  Where player.name = 'Christine Sinclair'
+
+  Group by match_id)
+;
