7bbf184ab522ef4e07a578a2c76637d0f9a0d485
Anton S <66568776+Arizoba@users.noreply.github.com>
2023-03-14 19:15:44 -0400

    finished

--- project3/Project3/src/Soccer.java
+++ project3/Project3/src/Soccer.java
@@ -132,27 +132,47 @@ class Soccer
                         "FROM\n" +
                         "    (SELECT MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, T2GOALS, COUNT(TICKET.MATCH_ID) AS TICKETS_SOLD\n" +
                         "     FROM\n" +
-                        "         (SELECT MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, COUNT(G2.COUNTRY) AS T2GOALS\n" +
-                        "          FROM\n" +
-                        "              (SELECT table2.MID, table2.COUNTRY1, table2.COUNTRY2, table2.ROUND, table2.DATE, COUNT(G1.COUNTRY) AS T1GOALS\n" +
-                        "               FROM\n" +
-                        "                   (SELECT table1.MATCH_ID AS MID, table1.COUNTRY1 AS COUNTRY1, MP.COUNTRY AS COUNTRY2, DATE, ROUND\n" +
-                        "                    FROM\n" +
-                        "                        (SELECT M.MATCH_ID, MIN(COUNTRY) AS COUNTRY1, M.DATE, M.ROUND\n" +
-                        "                         FROM MATCH AS M LEFT OUTER JOIN MATCH_PARTICIPANTS AS MP2\n" +
-                        "                                                         ON M.MATCH_ID = MP2.MATCH_ID\n" +
-                        "                         GROUP BY M.MATCH_ID, M.DATE, M.ROUND) table1 LEFT OUTER JOIN MATCH_PARTICIPANTS AS MP\n" +
-                        "                                                                                        ON table1.MATCH_ID = MP.MATCH_ID AND COUNTRY1 != MP.COUNTRY) table2\n" +
-                        "                       LEFT OUTER JOIN GOAL AS G1\n" +
-                        "                           ON table2.MID = G1.MATCH_ID AND table2.COUNTRY1 = G1.COUNTRY\n" +
-                        "               GROUP BY table2.MID, table2.COUNTRY1, table2.COUNTRY2, table2.ROUND, table2.DATE, G1.COUNTRY) table3\n" +
-                        "                  LEFT OUTER JOIN GOAL AS G2\n" +
-                        "                      ON MID = G2.MATCH_ID AND COUNTRY2 = G2.COUNTRY\n" +
-                        "          GROUP BY MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, G2.COUNTRY) table4\n" +
+                        "         ((SELECT MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, COUNT(G2.COUNTRY) AS T2GOALS\n" +
+                        "           FROM\n" +
+                        "               (SELECT table2.MID, table2.COUNTRY1, table2.COUNTRY2, table2.ROUND, table2.DATE, COUNT(G1.COUNTRY) AS T1GOALS\n" +
+                        "                FROM\n" +
+                        "                    (SELECT table1.MATCH_ID AS MID, table1.COUNTRY1 AS COUNTRY1, MP.COUNTRY AS COUNTRY2, DATE, ROUND\n" +
+                        "                     FROM\n" +
+                        "                         (SELECT M.MATCH_ID, MIN(COUNTRY) AS COUNTRY1, M.DATE, M.ROUND\n" +
+                        "                          FROM MATCH AS M LEFT OUTER JOIN MATCH_PARTICIPANTS AS MP2\n" +
+                        "                                                          ON M.MATCH_ID = MP2.MATCH_ID\n" +
+                        "                          where M.MATCH_ID in (select MATCH_ID from PLAYED_MATCH)\n" +
+                        "                          GROUP BY M.MATCH_ID, M.DATE, M.ROUND) table1 LEFT OUTER JOIN MATCH_PARTICIPANTS AS MP\n" +
+                        "                                                                                       ON table1.MATCH_ID = MP.MATCH_ID AND COUNTRY1 != MP.COUNTRY) table2\n" +
+                        "                        LEFT OUTER JOIN GOAL AS G1\n" +
+                        "                                        ON table2.MID = G1.MATCH_ID AND table2.COUNTRY1 = G1.COUNTRY\n" +
+                        "                GROUP BY table2.MID, table2.COUNTRY1, table2.COUNTRY2, table2.ROUND, table2.DATE, G1.COUNTRY) table3\n" +
+                        "                   LEFT OUTER JOIN GOAL AS G2\n" +
+                        "                                   ON MID = G2.MATCH_ID AND COUNTRY2 = G2.COUNTRY\n" +
+                        "           GROUP BY MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, G2.COUNTRY)\n" +
+                        "          union\n" +
+                        "          (SELECT MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, NULL AS T2GOALS\n" +
+                        "           FROM\n" +
+                        "               (SELECT table2.MID, table2.COUNTRY1, table2.COUNTRY2, table2.ROUND, table2.DATE, NULL AS T1GOALS\n" +
+                        "                FROM\n" +
+                        "                    (SELECT table1.MATCH_ID AS MID, table1.COUNTRY1 AS COUNTRY1, MP.COUNTRY AS COUNTRY2, DATE, ROUND\n" +
+                        "                     FROM\n" +
+                        "                         (SELECT M.MATCH_ID, MIN(COUNTRY) AS COUNTRY1, M.DATE, M.ROUND\n" +
+                        "                          FROM MATCH AS M LEFT OUTER JOIN MATCH_PARTICIPANTS AS MP2\n" +
+                        "                                                          ON M.MATCH_ID = MP2.MATCH_ID\n" +
+                        "                          where M.MATCH_ID not in (select MATCH_ID from PLAYED_MATCH)\n" +
+                        "                          GROUP BY M.MATCH_ID, M.DATE, M.ROUND) table1 LEFT OUTER JOIN MATCH_PARTICIPANTS AS MP\n" +
+                        "                                                                                       ON table1.MATCH_ID = MP.MATCH_ID AND COUNTRY1 != MP.COUNTRY) table2\n" +
+                        "                        LEFT OUTER JOIN GOAL AS G1\n" +
+                        "                                        ON table2.MID = G1.MATCH_ID AND table2.COUNTRY1 = G1.COUNTRY\n" +
+                        "                GROUP BY table2.MID, table2.COUNTRY1, table2.COUNTRY2, table2.ROUND, table2.DATE, G1.COUNTRY) table3\n" +
+                        "                   LEFT OUTER JOIN GOAL AS G2\n" +
+                        "                                   ON MID = G2.MATCH_ID AND COUNTRY2 = G2.COUNTRY\n" +
+                        "           GROUP BY MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, G2.COUNTRY))table4\n" +
                         "             LEFT OUTER JOIN TICKET\n" +
                         "                             ON MID = TICKET.MATCH_ID AND TICKET.STATUS = 'SOLD'\n" +
-                        "     GROUP BY MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, T2GOALS, TICKET.MATCH_ID)" +
-                        "WHERE COUNTRY1 = '" + country + "' OR COUNTRY2 = '" + country + "'";
+                        "     GROUP BY MID, COUNTRY1, COUNTRY2, ROUND, DATE, T1GOALS, T2GOALS, TICKET.MATCH_ID)\n" +
+                        "WHERE COUNTRY1 = '"+country+"' OR COUNTRY2 = '"+country+"'";
                 java.sql.ResultSet rs = statement.executeQuery(querySQL);
                 System.out.print("\n-----------------------------------------------------------------------------------" +
                         "-------------------------------");
@@ -166,10 +186,11 @@ class Soccer
                     String country2 = rs.getString("Country2");
                     String round = rs.getString("Round");
                     String date = rs.getString("Date");
-                    int t1Goals = rs.getInt("T1Goals");
-                    int t2Goals = rs.getInt("T2Goals");
+                    String t1Goals = rs.getString("T1Goals");
+                    String t2Goals = rs.getString("T2Goals");
                     int ticketsSold = rs.getInt("Tickets_Sold");
-                    System.out.printf("\n%-16s %-16s %-16s %-15s %-16d %-16d %-15d", country1, country2, round, date, t1Goals, t2Goals,
+
+                    System.out.printf("\n%-16s %-16s %-16s %-15s %-16s %-16s %-15d", country1, country2, round, date, t1Goals, t2Goals,
                             ticketsSold);
                     rowCount++;
                 }
